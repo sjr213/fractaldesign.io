a7fec13d300e55f377f2f4d355a0415686bd2485
Steve Robles <sjrobles@yahoo.com>
2026-08-15 11:58:54 -0400

    Update for 8/15/26

--- scripts.js
+++ scripts.js
@@ -18,10 +18,10 @@ let firstAttractorsTime = false;
 
 // First index slides start at 
 // ranges from 0 to number of slides - 4
-let firstIndex3D = 152;                // current max = 15 x 12 = 180 - 4 = 176
-let firstIndexFractals = 164;          // current max = 38 x 12 = 456 - 4 = 452
-let firstIndexAutomata = 152;          // current max = 35 x 12 = 420 - 4 = 404
-let firstIndexAttractors = 152;        // current max = 30 x 12 = 360 - 4 = 356
+let firstIndex3D = 156;                // current max = 15 x 12 = 180 - 4 = 176
+let firstIndexFractals = 168;          // current max = 38 x 12 = 456 - 4 = 452
+let firstIndexAutomata = 156;          // current max = 35 x 12 = 420 - 4 = 404
+let firstIndexAttractors = 156;        // current max = 30 x 12 = 360 - 4 = 356
 
 let _groupsFractal3D;
 let _groupsFractals;
